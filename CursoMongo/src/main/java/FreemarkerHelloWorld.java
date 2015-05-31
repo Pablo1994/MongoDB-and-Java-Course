fee571c95afbe02c98a864cd3b8b9b686b671c86
Pablo1994 <jpaz199448@gmail.com>
2015-05-30 21:09:26 -0600

    Hola

--- CursoMongo/src/main/java/FreemarkerHelloWorld.java
+++ CursoMongo/src/main/java/FreemarkerHelloWorld.java
@@ -1,4 +1,12 @@
 import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by pablo on 30/05/15.
@@ -7,5 +15,18 @@ public class FreemarkerHelloWorld {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         configuration.setClassForTemplateLoading(FreemarkerHelloWorld.class, "/");
+        try {
+            Template helloTemplate = configuration.getTemplate("hello.ftl");
+            StringWriter writer = new StringWriter();
+            Map<String, Object> helloMap = new HashMap<String, Object>();
+            helloMap.put("name", "Pablo");
+
+            helloTemplate.process(helloMap,writer);
+            System.out.println(writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
     }
 }
